aa6a315a0a7fe2af0a23b577d7c95e896e47dc5a
nathawat.singhrahul <nathawat.singhrahul@gmail.com>
2022-03-23 13:47:54 +0530

    Created recruitment service

---
+++ recruitment-service/src/main/java/com/stackroute/recruitmentservice/RecruitmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.recruitmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentServiceApplication.class, args);
+	}
+
+}
