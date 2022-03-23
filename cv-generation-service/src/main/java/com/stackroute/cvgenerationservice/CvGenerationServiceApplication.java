483a371af94cf1dad8529b61e5c114ad2af02033
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2022-03-23 16:39:32 +0530

    created service

---
+++ cv-generation-service/src/main/java/com/stackroute/cvgenerationservice/CvGenerationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.cvgenerationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvGenerationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvGenerationServiceApplication.class, args);
+	}
+
+}
