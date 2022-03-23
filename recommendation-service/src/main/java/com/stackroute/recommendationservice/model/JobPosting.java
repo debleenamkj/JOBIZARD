dcf0c7546990fee8b755c1e97fbc4eace455926a
malumalathi032@gmail.com <malumalathi032@gmail.com>
2022-03-23 14:51:20 +0530

    model classes created

--- recommendation-service/src/main/java/com/stackroute/recommendationservice/model/JobPosting.java
+++ recommendation-service/src/main/java/com/stackroute/recommendationservice/model/JobPosting.java
@@ -1,4 +1,21 @@
 package com.stackroute.recommendationservice.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.NodeEntity;
+
+import java.util.ArrayList;
+
+@NodeEntity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class JobPosting {
+    @Id
+    private long jobId;
+    private ArrayList skillsRequired;
+    private String jobRole;
 }
--- recommendation-service/src/main/java/com/stackroute/recommendationservice/model/User.java
+++ recommendation-service/src/main/java/com/stackroute/recommendationservice/model/User.java
@@ -1,6 +1,21 @@
 package com.stackroute.recommendationservice.model;
 
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.NodeEntity;
+
+import java.util.ArrayList;
+
+@NodeEntity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class User {
-
-
+  @Id
+  private String email;
+  private ArrayList jobPreferences;
+  private ArrayList skillSet;
 }
