38894c796b85389d1eee0433be2d10efab49a28e
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2022-03-23 22:16:42 +0530

    repo layer is done

--- cv-generation-service/src/main/java/com/stackroute/cvgenerationservice/domain/userCv.java
+++ cv-generation-service/src/main/java/com/stackroute/cvgenerationservice/domain/userCv.java
@@ -1,5 +1,6 @@
 package com.stackroute.cvgenerationservice.domain;
 
+
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
--- cv-generation-service/src/main/java/com/stackroute/cvgenerationservice/repository/repository.java
+++ cv-generation-service/src/main/java/com/stackroute/cvgenerationservice/repository/repository.java
@@ -2,6 +2,8 @@ package com.stackroute.cvgenerationservice.repository;
 
 import com.stackroute.cvgenerationservice.domain.userCv;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface repository extends MongoRepository<userCv,String> {
 }
