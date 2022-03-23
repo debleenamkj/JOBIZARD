c208a23908959a60d5557d1528120d1d98ebb299
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2022-03-23 15:12:20 +0530

    created service

---
+++ CvGeneration/src/main/java/com/stackroute/CvGeneration/CvGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.CvGeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvGenerationApplication.class, args);
+	}
+
+}
---
+++ CvGeneration/src/test/java/com/stackroute/CvGeneration/CvGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.CvGeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
