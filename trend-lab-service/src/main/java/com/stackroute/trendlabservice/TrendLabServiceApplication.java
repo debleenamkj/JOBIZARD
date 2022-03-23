02d9aadaa7ef6c69d89fe9d5baff89993ddad3dc
debleenamkj <99349517+debleenamkj@users.noreply.github.com>
2022-03-23 12:47:28 +0530

    Added the Trend Lab Microservice

---
+++ trend-lab-service/src/main/java/com/stackroute/trendlabservice/TrendLabServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.trendlabservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendLabServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendLabServiceApplication.class, args);
+	}
+
+}
---
+++ trend-lab-service/src/test/java/com/stackroute/trendlabservice/TrendLabServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.trendlabservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendLabServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
