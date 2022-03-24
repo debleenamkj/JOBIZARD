b264711f811a20483aaf75beed4fd256041a772c
debleenamkj <99349517+debleenamkj@users.noreply.github.com>
2022-03-24 13:20:42 +0530

    Added external API for Salary Trend

--- trend-lab-service/src/test/java/com/stackroute/trendlabservice/TrendLabServiceApplicationTests.java
+++ trend-lab-service/src/test/java/com/stackroute/trendlabservice/TrendLabServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TrendLabServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
