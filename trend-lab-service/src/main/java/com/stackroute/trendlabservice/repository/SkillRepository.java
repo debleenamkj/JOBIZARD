65bef2da399159c4039ff8d9e83107e745eb9376
debleenamkj <99349517+debleenamkj@users.noreply.github.com>
2022-03-25 14:15:20 +0530

    Added Model Class Skill Trends

--- trend-lab-service/src/main/java/com/stackroute/trendlabservice/repository/SkillRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stackroute.trendlabservice.repository;
-
-import com.stackroute.trendlabservice.model.SkillTrend;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SkillRepository extends MongoRepository<SkillTrend,Long> {
-}
