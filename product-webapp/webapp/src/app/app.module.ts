2986530c614cf15a7db0727054820868cc781447
nathawat.singhrahul <nathawat.singhrahul@gmail.com>
2022-03-29 12:55:22 +0530

    completed recuritment service

--- product-webapp/webapp/src/app/app.module.ts
+++ product-webapp/webapp/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { RegisterComponent } from './register/register.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { JobDetailComponent } from './job-detail/job-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
     NavBarComponent,
-    LoginComponent
+    LoginComponent,
+    JobDetailComponent
   ],
   imports: [
     BrowserModule,
