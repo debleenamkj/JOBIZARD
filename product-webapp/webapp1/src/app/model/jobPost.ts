f328dab30bdb202551d2ab6bd0049856dcb73ad7
malumalathi032@gmail.com <malumalathi032@gmail.com>
2022-03-30 11:40:45 +0530

    updated

---
+++ product-webapp/webapp1/src/app/model/jobPost.ts
@@ -0,0 +1,23 @@
+export class jobPost{
+    companyName:string="";
+    companyUrl:string="";
+    companyEmail:string="";
+    industryType:string="";
+    jobRole:string="";
+    salary:any="";
+    jobDescription:string="";
+    location:string="";
+    lastdate:string="";
+
+    constructor(){
+        this.companyName="";
+        this.companyUrl="";
+        this.companyEmail="";
+        this.industryType="";
+        this.jobRole="";
+        this.salary="";
+        this.jobDescription="";
+        this.location="";
+        this.lastdate="";
+    }
+}
