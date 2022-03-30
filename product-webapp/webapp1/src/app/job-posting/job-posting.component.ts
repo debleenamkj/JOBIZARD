2bbb883757920c108667742bd35d9ad25c9ff1cf
malumalathi032@gmail.com <malumalathi032@gmail.com>
2022-03-30 09:30:25 +0530

    components added in webapp1

---
+++ product-webapp/webapp1/src/app/job-posting/job-posting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-job-posting',
+  templateUrl: './job-posting.component.html',
+  styleUrls: ['./job-posting.component.css']
+})
+export class JobPostingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
