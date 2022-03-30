f5b198210c0e890d421c970fc6b12c1d25d2a6e3
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2022-03-30 11:47:10 +0530

    cv component

--- product-webapp/webapp1/src/app/app.module.ts
+++ product-webapp/webapp1/src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CvGenerationComponent } from './cv-generation/cv-generation.component';
+// import { CvFormComponent } from './cv-form/cv-form.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CvGenerationComponent
+    CvGenerationComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
