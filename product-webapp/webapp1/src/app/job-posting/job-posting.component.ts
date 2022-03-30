b650b01637c77dcf6a162039be290accee73d7a8
malumalathi032@gmail.com <malumalathi032@gmail.com>
2022-03-30 09:40:03 +0530

    posting jobs designed

--- product-webapp/webapp1/src/app/job-posting/job-posting.component.ts
+++ product-webapp/webapp1/src/app/job-posting/job-posting.component.ts
@@ -11,5 +11,24 @@ export class JobPostingComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  company(){
+    let div = document.getElementsByClassName('b1') as HTMLCollectionOf<HTMLElement>;
+    div[0].style.backgroundColor='yellow';
+   }
+
+   job(){
+    let div = document.getElementsByClassName('b2') as HTMLCollectionOf<HTMLElement>;
+    div[0].style.backgroundColor='#FAC710';
+   }
+   requirements(){
+    let div = document.getElementsByClassName('b3') as HTMLCollectionOf<HTMLElement>;
+    div[0].style.backgroundColor='#CEE741';
+   }
+   finish(){
+     console.log("finish");
+    let div = document.getElementsByClassName('b4') as HTMLCollectionOf<HTMLElement>;
+    div[0].style.backgroundColor='#8FD14F';
+   }
+   
 
 }
