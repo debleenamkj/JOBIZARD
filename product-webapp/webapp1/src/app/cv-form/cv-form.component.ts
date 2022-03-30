6cbe2cfc51bc08ab5814a4e4ebd32b0163b367d2
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2022-03-30 11:50:56 +0530

    cv component

--- product-webapp/webapp1/src/app/cv-form/cv-form.component.ts
+++ product-webapp/webapp1/src/app/cv-form/cv-form.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-cv-form',
-  templateUrl: './cv-form.component.html',
-  styleUrls: ['./cv-form.component.css']
-})
-export class CvFormComponent implements OnInit {
+// @Component({
+//   selector: 'app-cv-form',
+//   templateUrl: './cv-form.component.html',
+//   styleUrls: ['./cv-form.component.css']
+// })
+// export class CvFormComponent implements OnInit {
 
-  constructor() { }
+//   constructor() { }
 
-  ngOnInit(): void {
-  }
+//   ngOnInit(): void {
+//   }
 
-}
+// }
 
