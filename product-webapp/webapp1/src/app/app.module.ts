65314f39c681896f15c847fb4f095e293c781b12
debleenamkj <99349517+debleenamkj@users.noreply.github.com>
2022-03-30 14:10:30 +0530

    Changed Trends UI

--- product-webapp/webapp1/src/app/app.module.ts
+++ product-webapp/webapp1/src/app/app.module.ts
@@ -6,37 +6,49 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CvGenerationComponent } from './cv-generation/cv-generation.component';
 // import { CvFormComponent } from './cv-form/cv-form.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+
+import { JobApplyComponent } from './job-apply/job-apply.component';
+import { JobPostingComponent } from './job-posting/job-posting.component';
 
 import { JobDetailComponent } from './job-detail/job-detail.component';
-// import {MatButtonModule} from '@angular/material/button';
 
 
 import { TrendLabComponent } from './trend-lab/trend-lab.component';
 import { SkillsTrendLabComponent } from './skills-trend-lab/skills-trend-lab.component';
 import { SalaryTrendLabComponent } from './salary-trend-lab/salary-trend-lab.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { ChatbotComponent } from './chatbot/chatbot.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LayoutModule } from '@angular/cdk/layout';
+
+//Angular Material Imports
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import {MatSelectModule} from '@angular/material/select';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatRippleModule,MatNativeDateModule} from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
-import { JobApplyComponent } from './job-apply/job-apply.component';
-import { JobPostingComponent } from './job-posting/job-posting.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CvGenerationComponent,
-<<<<<<< HEAD
-=======
     JobDetailComponent,
     TrendLabComponent,
     SkillsTrendLabComponent,
@@ -46,19 +58,12 @@ import { JobPostingComponent } from './job-posting/job-posting.component';
     JobApplyComponent,
     JobPostingComponent
 
->>>>>>> 56f5ea69e37c9fdb3ed063bbba51b17dcb8759cf
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-<<<<<<< HEAD
-    MatFormFieldModule,
-    MatInputModule
-=======
-
     MatButtonModule,
-
     ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
@@ -67,9 +72,22 @@ import { JobPostingComponent } from './job-posting/job-posting.component';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
-
->>>>>>> 56f5ea69e37c9fdb3ed063bbba51b17dcb8759cf
+    MatListModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatChipsModule,
+    MatTooltipModule,
+    MatMenuModule,
+    MatCardModule,
+    MatGridListModule,
+    MatRippleModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent],
