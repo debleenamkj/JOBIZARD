e3168e0d7a6b6f811e291bd80c4231d3010f4559
debleenamkj <99349517+debleenamkj@users.noreply.github.com>
2022-04-12 13:07:02 +0530

    Jobseeker Profile Completed

---
+++ trend-lab-service/src/test/java/com/stackroute/trendlabservice/repository/SkillTrendRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.stackroute.trendlabservice.repository;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@DataMongoTest
+public class SkillTrendRepositoryTest {
+}
