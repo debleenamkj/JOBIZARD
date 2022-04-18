01758e633c064947895f460235a66666c79aaf30
nathawat.singhrahul <nathawat.singhrahul@gmail.com>
2022-04-18 16:32:18 +0530

    UI of profile update

---
+++ product-webapp/webapp1/src/app/model/user-details.ts
@@ -0,0 +1,39 @@
+// import {UserLogin} from "./userLogin"
+// public user:UserLogin
+export class UserDetails {
+    constructor(
+       public firstName:string,
+       public lastName:string,
+       public gender:string,
+       public dateOfBirth:string,
+       public mobileNumber:string,
+       public profilePicture:any,
+       public lane:string,
+       public state:string,
+       public city:string,
+       public pincode:string,
+       public Nationality:string,
+       public academicCertification:string[],
+       public skillSet:string[],
+       public jobPreference:string[],
+       public achievements:string[],
+       
+
+       
+    ){
+        
+    }
+    
+
+}
+
+// // 
+// export class Person {
+//  constructor(
+//   public name: string,
+//    public lastName: string,
+//    public age:number
+  
+//   ) {}
+// }
+// 
