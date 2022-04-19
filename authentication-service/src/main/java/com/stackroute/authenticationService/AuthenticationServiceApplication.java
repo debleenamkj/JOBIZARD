db7bf8515f0c0a184da80fe9a1346be3c4475ca3
shrutipawan2909@gmail.com <shrutipawan2909@gmail.com>
2022-04-19 16:47:36 +0530

    eureka server

--- authentication-service/src/main/java/com/stackroute/authenticationService/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/stackroute/authenticationService/AuthenticationServiceApplication.java
@@ -4,9 +4,11 @@ import com.stackroute.authenticationService.filter.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AuthenticationServiceApplication
 {
 	public static void main(String[] args)
