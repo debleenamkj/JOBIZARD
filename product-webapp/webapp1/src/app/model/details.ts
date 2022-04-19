196ee72ba58ac303334018e9b0ce8267df3933b9
malumalathi032@gmail.com <malumalathi032@gmail.com>
2022-04-19 13:19:48 +0530

    update-user

---
+++ product-webapp/webapp1/src/app/model/details.ts
@@ -0,0 +1,13 @@
+export class details{
+        public academicCertification:string[]=[];
+       public skillSet:string[]=[];
+       public jobPreference:string[]=[];
+       public achievements:string[]=[]
+
+       constructor(){
+        this.academicCertification=[],
+        this.skillSet=[],
+        this.jobPreference=[],
+        this.achievements=[]
+       }
+}
