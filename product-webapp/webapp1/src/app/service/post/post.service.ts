363eaac92a496061f98b01d0d4fae19a13839645
Mohammed Zakaria <mohammed.zakaria@niit.com>
2022-04-20 15:45:15 +0530

    Minor Changes

--- product-webapp/webapp1/src/app/service/post/post.service.ts
+++ product-webapp/webapp1/src/app/service/post/post.service.ts
@@ -1,46 +1,52 @@
 import { HttpClient } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
 import { Injectable } from '@angular/core';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  loginUser=localStorage.getItem('loginId');
 
-  selectedSeekerEmail="a@a.com";
 
-  sendImagePost(email:string,data:any){
-    return  this.httpClient.post('http://localhost:9093/api/v1/post/image/'+email,data);
+
+  loginUser = localStorage.getItem('loginId');
+
+  selectedSeekerEmail = "a@a.com";
+
+  sendImagePost(email: string, data: any) {
+    return this.httpClient.post('http://localhost:9093/api/v1/post/image/' + email, data);
   }
 
-  sendBlogPost(email:string,data:any){
-    return this.httpClient.post('http://localhost:9093/api/v1/post/blog/m1@gmail.com',data);
+  sendBlogPost(email: string, data: any) {
+    return this.httpClient.post('http://localhost:9093/api/v1/post/blog/m1@gmail.com', data);
   }
 
-  addReviewInImage(postId:string,data:any){
-    return this.httpClient.put('http://localhost:9093/api/v1/post/addReviews/post/'+postId,data);
+  addReviewInImage(postId: string, data: any) {
+    return this.httpClient.put('http://localhost:9093/api/v1/post/addReviews/post/' + postId, data);
   }
 
-  addLikeInImage(postId:string){
-    return this.httpClient.put('http://localhost:9093/api/v1/post/addLike/post/'+postId+"/"+this.loginUser,null);
+  addLikeInImage(postId: string) {
+    return this.httpClient.put('http://localhost:9093/api/v1/post/addLike/post/' + postId + "/" + this.loginUser, null);
   }
 
-  getAllPost(){
+  getAllPost() {
     return this.httpClient.get('http://localhost:9093/api/v1/post/getAllPost')
   }
 
-  addLikeInBlog(postId:string){
-    return this.httpClient.put('http://localhost:9093/api/v1/post/addLike/blog/'+postId+"/"+this.loginUser,null);
+  addLikeInBlog(postId: string) {
+    return this.httpClient.put('http://localhost:9093/api/v1/post/addLike/blog/' + postId + "/" + this.loginUser, null);
   }
 
-  addReviewInBlog(postId:string,data:any){
-    return this.httpClient.put('http://localhost:9093/api/v1/post/addReviews/blog/'+postId,data)
+  addReviewInBlog(postId: string, data: any) {
+    return this.httpClient.put('http://localhost:9093/api/v1/post/addReviews/blog/' + postId, data)
   }
 
-  getSeeker(emailId:any){
-    return this.httpClient.get('http://localhost:8098/api/v1/'+emailId)
+  getSeeker(emailId: any) {
+    return this.httpClient.get('http://localhost:8098/api/v1/' + emailId)
   }
 }
--- product-webapp/webapp1/src/environments/environment.prod.ts
+++ product-webapp/webapp1/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  local: 'production',
+  apiBaseUrl: 'http://13.233.243.17:8080',
 };
