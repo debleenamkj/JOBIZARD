a905f92324f1cc4cffc9dcecd4bff202d2a67be9
Mohammed Zakaria <mohammed.zakaria@niit.com>
2022-04-20 16:32:04 +0530

    Added Static Folder

--- recruitment-service/src/main/java/com/stackroute/recruitmentservice/controller/JobController.java
+++ recruitment-service/src/main/java/com/stackroute/recruitmentservice/controller/JobController.java
@@ -13,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-@CrossOrigin("*")
+//@CrossOrigin("*")
 @RestController
 @RequestMapping("/recruitment")
 public class JobController {
