6e52d140ce4e11ed1e90b4baea79f706c629c4ed
Mohammed Zakaria <mohammed.zakaria@niit.com>
2022-04-20 10:38:17 +0530

    Minor Changes

--- authentication-service/src/test/java/com/stackroute/authenticationService/AuthenticationServiceApplicationTests.java
+++ authentication-service/src/test/java/com/stackroute/authenticationService/AuthenticationServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.stackroute.authenticationService;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AuthenticationServiceApplicationTests {
-
-//	@Test
-//	void contextLoads() {
-//	}
-
-}
+//package com.stackroute.authenticationService;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class AuthenticationServiceApplicationTests {
+//
+////	@Test
+////	void contextLoads() {
+////	}
+//
+//}
