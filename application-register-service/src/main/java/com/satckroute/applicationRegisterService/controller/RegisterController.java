62e280de682359059191ce4a669e116ad83e0375
malumalathi032@gmail.com <malumalathi032@gmail.com>
2022-04-23 20:06:13 +0530

    modified little

--- application-register-service/src/main/java/com/satckroute/applicationRegisterService/controller/RegisterController.java
+++ application-register-service/src/main/java/com/satckroute/applicationRegisterService/controller/RegisterController.java
@@ -17,7 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-@CrossOrigin("*")
+//@CrossOrigin("*")
 @Slf4j
 @RestController
 @RequestMapping("api/v1")
