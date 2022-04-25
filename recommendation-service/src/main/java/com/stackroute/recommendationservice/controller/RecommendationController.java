347228f10eeaefa204d700ad9cfc4ccba12f0ccd
malumalathi032@gmail.com <malumalathi032@gmail.com>
2022-04-25 16:03:13 +0530

    modified

--- recommendation-service/src/main/java/com/stackroute/recommendationservice/controller/RecommendationController.java
+++ recommendation-service/src/main/java/com/stackroute/recommendationservice/controller/RecommendationController.java
@@ -15,7 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-//@CrossOrigin("*")
+@CrossOrigin("*")
 @RestController
 @RequestMapping("api/v1/recommend")
 @Slf4j
