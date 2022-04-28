b06b247581a44699989e19ad4669469d9e041499
debleenamkj <99349517+debleenamkj@users.noreply.github.com>
2022-04-28 11:55:14 +0530

    Final Changes

--- product-webapp/webapp1/src/app/service/skilltest-service.service.ts
+++ product-webapp/webapp1/src/app/service/skilltest-service.service.ts
@@ -9,10 +9,10 @@ export class SkilltestServiceService {
 
   constructor(private httpClient:HttpClient) { }
 
-  // baseUrl='https://jobizard.stackroute.io' + '/application-register-service'
-  // baseUrl1='https://jobizard.stackroute.io' + '/assessment-service'
-    baseUrl='http://localhost:8098'
-    baseUrl1='http://localhost:8081'
+  baseUrl='https://jobizard.stackroute.io' + '/application-register-service'
+  baseUrl1='https://jobizard.stackroute.io' + '/assessment-service'
+    // baseUrl='http://localhost:8098'
+    // baseUrl1='http://localhost:8081'
 
 
   answeredQuestions:number = 0;
